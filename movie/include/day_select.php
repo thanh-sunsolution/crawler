5a72b20847f467b08cc28f922bc2016e75a4bec0
thanh-sunsolution <thanh.sunsolution@gmail.com>
2021-06-17 17:33:13 +0700

    v1

--- movie/include/day_select.php
+++ movie/include/day_select.php
@@ -3,8 +3,7 @@
     require_once('simple_html_dom.php'); 
 
     $id = $_GET['id'];
-    //$url = 'https://moveek.com/cinema/showtime/'.$id.'?date=&header=1';
-    $url = 'https://moveek.com/cinema/showtime/'.$id.'?date=2021-06-19&header=1';
+    $url = 'https://moveek.com/cinema/showtime/'.$id.'?date=&header=1';
     $content = file_get_html2($url);
 
     // foreach($content->find('.showtimes .showtime-dates') as $day){
--- movie/include/result.php
+++ movie/include/result.php
@@ -4,7 +4,7 @@
 
     $id = $_GET['id'];
 
-    $url = 'https://moveek.com/cinema/showtime/'.$id.'?date='.date('Y-m-d').'&header=1';
+    $url = 'https://moveek.com/cinema/showtime/'.$id.'?date=&header=1';
     $content = file_get_html2($url);
 
     foreach($content->find('.showtimes .showtime-dates') as $day){
@@ -14,11 +14,16 @@
     foreach($content->find('.showtimes .card-sm') as $list){
         echo $list;
     }
+    
 
 ?>
+<div id="day-display"></div>
 
 <script>
-
-
-
+    $('[class="date"]').on('change',function(){
+        var url='include/day_select.php'+this.value; //construct url and fetch only part of page
+        $("#day-display").load(url,function(){
+        //Anything you want do after contents are loaded
+        });
+    })
 </script>
