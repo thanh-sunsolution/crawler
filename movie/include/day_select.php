8fc15985fbeadeb4aea182bf783845f140caa5fb
thanh-sunsolution <thanh.sunsolution@gmail.com>
2021-06-23 09:31:18 +0700

    fix plaintext

--- movie/include/day_select.php
+++ movie/include/day_select.php
@@ -12,36 +12,35 @@
 
 
     foreach($content->find('.showtimes .card-sm') as $list){
-
         
         $img = $list->find('.card-body img.img-fluid', 0);
 
-        $title = $list->find('.card-body h4.card-title', 0)->plaintext;
+        $title = $list->find('.card-body .card-title', 0);
         
         $trailer = $list->find('.card-body .card-text', 0);
 
         $type = $list->find('.mt-2 label', 0);
 
         $time = $list->find('.mt-2 span', 0);
+
+
             
-        if (isset($title)) {
-                echo
-                    "<tr>
-                        <td>$img</td>
-                        <td>
-                            <table>
-                                <tr>
-                                    <td>$title</td>
-                                    <td>$trailer</td>
-                                    <td>$type</td>
-                                    <td>$time</td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>";
-            } else {
-                echo "Chưa có phim chiếu vào ngày này. Xin cảm ơn!";
-            }
+        if (isset($img)) {
+            echo
+                "<tr>
+                    <td>$img</td>
+                    <td>
+                        <table>
+                            <tr><td>$title->plaintext</td></tr>
+                            <tr><td>$trailer</td></tr>
+                            <tr><td>$type</td></tr>
+                            <tr><td>$time</td></tr>
+                        </table>
+                    </td>
+                </tr>";
+        } else {
+            echo "Chưa có phim chiếu vào ngày này. Xin cảm ơn!";
+        }
       
 
     } 
